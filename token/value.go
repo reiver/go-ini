3db938f9b2a474ab360af7642152aaa0aa422812
Charles Iliya Krempeaux <iamreiver@gmail.com>
2018-07-11 01:32:43 -0700

    initoken.Value

---
+++ token/value.go
@@ -0,0 +1,36 @@
+package initoken
+
+import (
+	"io"
+)
+
+type Value struct {
+	value string
+}
+
+func SomeValue(s string) Value {
+	return Value{value: s}
+}
+
+func (receiver Value) INIToken() Type {
+	return receiver
+}
+
+func (receiver Value) INITokenSome() SomeType {
+	return receiver
+}
+
+func (Value) INITokenValue() {
+	// Nothing here.
+}
+
+func (receiver Value) String() string {
+	return string(receiver.value)
+}
+
+func (receiver Value) WriteTo(writer io.Writer) (int64, error) {
+	n, err := io.WriteString(writer, receiver.value)
+	n64 := int64(n)
+
+	return n64, err
+}
