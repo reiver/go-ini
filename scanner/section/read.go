e49d8a3b3ba5f3f2a9355b766e665e684458a081
Charles Iliya Krempeaux <iamreiver@gmail.com>
2018-07-11 11:43:41 -0700

    iniscanner_section

---
+++ scanner/section/read.go
@@ -0,0 +1,61 @@
+package iniscanner_section
+
+import (
+	"github.com/reiver/go-ini/scanner/error"
+	"github.com/reiver/go-ini/token"
+
+	"bytes"
+	"io"
+)
+
+
+func Read(runeScanner io.RuneScanner) (initoken.Section, int, error) {
+	if nil == runeScanner  {
+		return initoken.Section{}, 0, iniscanner_error.NilRuneScanner
+	}
+
+	var buffer bytes.Buffer
+
+	var n int
+
+	var notFirst bool
+
+	for {
+		r, n2, err := runeScanner.ReadRune()
+		n += n2
+		if nil != err && io.EOF != err {
+			return initoken.SomeSection( buffer.String() ), n, iniscanner_error.InternalError(
+				buffer.String(),
+				"trying to read rune",
+				err,
+			)
+		}
+		if io.EOF == err {
+			return initoken.Section{}, n, iniscanner_error.SyntaxError(
+				"not a section, sections end with a \"]\" charcter",
+				string(r),
+			)
+		}
+
+		if !notFirst {
+			notFirst = true
+			switch r {
+			case '[':
+				// Nothing here.
+			default:
+				return initoken.Section{}, n, iniscanner_error.SyntaxError(
+					"not a section, sections begin with a \"[\" charcter",
+					string(r),
+				)
+			}
+		}
+
+		if ']' == r {
+			buffer.WriteRune(r)
+
+			return initoken.SomeSection( buffer.String() ), n, nil
+		}
+
+		buffer.WriteRune(r)
+	}
+}
---
+++ scanner/section/read_test.go
@@ -0,0 +1,210 @@
+package iniscanner_section
+
+import (
+	"github.com/reiver/go-utf8s"
+
+	"io"
+	"strings"
+
+	"testing"
+)
+
+func TestRead(t *testing.T) {
+
+	tests := []struct{
+		Value         string
+		ExpectedValue string
+		ExpectedSize  int
+	}{
+		{
+			Value:            "[the section]",
+			ExpectedValue:    "[the section]",
+			ExpectedSize: len("[the section]"),
+		},
+
+
+
+		{
+			Value:            "[the section]\n",
+			ExpectedValue:    "[the section]",
+			ExpectedSize: len("[the section]"),
+		},
+		{
+			Value:            "[the section]\r",
+			ExpectedValue:    "[the section]",
+			ExpectedSize: len("[the section]"),
+		},
+		{
+			Value:            "[the section]\r\n",
+			ExpectedValue:    "[the section]",
+			ExpectedSize: len("[the section]"),
+		},
+		{
+			Value:            "[the section]\v",
+			ExpectedValue:    "[the section]",
+			ExpectedSize: len("[the section]"),
+		},
+		{
+			Value:            "[the section]\u0085",
+			ExpectedValue:    "[the section]",
+			ExpectedSize: len("[the section]"),
+		},
+		{
+			Value:            "[the section]\u2028",
+			ExpectedValue:    "[the section]",
+			ExpectedSize: len("[the section]"),
+		},
+		{
+			Value:            "[the section]\u2029",
+			ExpectedValue:    "[the section]",
+			ExpectedSize: len("[the section]"),
+		},
+
+
+
+		{
+			Value:            "[the section]\napple=one",
+			ExpectedValue:    "[the section]",
+			ExpectedSize: len("[the section]"),
+		},
+		{
+			Value:            "[the section]\rapple=one",
+			ExpectedValue:    "[the section]",
+			ExpectedSize: len("[the section]"),
+		},
+		{
+			Value:            "[the section]\r\napple=one",
+			ExpectedValue:    "[the section]",
+			ExpectedSize: len("[the section]"),
+		},
+		{
+			Value:            "[the section]\vapple=one",
+			ExpectedValue:    "[the section]",
+			ExpectedSize: len("[the section]"),
+		},
+		{
+			Value:            "[the section]\u0085apple=one",
+			ExpectedValue:    "[the section]",
+			ExpectedSize: len("[the section]"),
+		},
+		{
+			Value:            "[the section]\u2028apple=one",
+			ExpectedValue:    "[the section]",
+			ExpectedSize: len("[the section]"),
+		},
+		{
+			Value:            "[the section]\u2029apple=one",
+			ExpectedValue:    "[the section]",
+			ExpectedSize: len("[the section]"),
+		},
+
+
+
+		{
+			Value:            "[the section]\napple=one\n",
+			ExpectedValue:    "[the section]",
+			ExpectedSize: len("[the section]"),
+		},
+		{
+			Value:            "[the section]\rapple=one\r",
+			ExpectedValue:    "[the section]",
+			ExpectedSize: len("[the section]"),
+		},
+		{
+			Value:            "[the section]\r\napple=one\r\n",
+			ExpectedValue:    "[the section]",
+			ExpectedSize: len("[the section]"),
+		},
+		{
+			Value:            "[the section]\vapple=one\v",
+			ExpectedValue:    "[the section]",
+			ExpectedSize: len("[the section]"),
+		},
+		{
+			Value:            "[the section]\u0085apple=one\u0085",
+			ExpectedValue:    "[the section]",
+			ExpectedSize: len("[the section]"),
+		},
+		{
+			Value:            "[the section]\u2028apple=one\u2028",
+			ExpectedValue:    "[the section]",
+			ExpectedSize: len("[the section]"),
+		},
+		{
+			Value:            "[the section]\u2029apple=one\u2029",
+			ExpectedValue:    "[the section]",
+			ExpectedSize: len("[the section]"),
+		},
+	}
+
+
+	for testNumber, test := range tests {
+
+		runeScanner := utf8s.NewRuneScanner( strings.NewReader(test.Value) )
+
+		token, actualSize, err := Read(runeScanner)
+		if nil != err && io.EOF != err {
+			t.Errorf("For test #%d, did not expect an error, but actually got one: (%T) %q.", testNumber, err, err)
+			continue
+		}
+
+		if expected, actual := test.ExpectedValue, token.String(); expected != actual {
+			t.Errorf("For test #%d, expected %q, but actually got %q.", testNumber, expected, actual)
+			continue
+		}
+		if expected, actual := test.ExpectedSize, actualSize; expected != actual {
+			t.Errorf("For test #%d, expected %d, but actually got %d.", testNumber, expected, actual)
+			continue
+		}
+
+		if io.EOF == err {
+			continue
+		}
+	}
+}
+
+func TestReadError(t *testing.T) {
+
+	tests := []struct{
+		Value string
+	}{
+		{
+			Value: "section]",
+		},
+
+
+
+		{
+			Value: "; this is a comment.",
+		},
+		{
+			Value: "# this is a comment.",
+		},
+		{
+			Value: "key=value",
+		},
+		{
+			Value: "key:value",
+		},
+		{
+			Value: "    \r\n",
+		},
+
+
+
+		{
+			Value: "    [section]",
+		},
+	}
+
+	for testNumber, test := range tests {
+
+		runeScanner := utf8s.NewRuneScanner( strings.NewReader(test.Value) )
+
+		_, _, err := Read(runeScanner)
+		if nil == err || io.EOF == err {
+			t.Errorf("For test #%d, expected an error, but did not actually got one: (%T) %q.", testNumber, err, err)
+			continue
+		}
+	}
+}
