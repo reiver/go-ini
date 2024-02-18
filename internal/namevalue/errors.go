2d9fd3429a51cc23c20467995a7f251ca256e892
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-02-18 13:47:13 -0800

    ininamevalue

---
+++ internal/namevalue/errors.go
@@ -0,0 +1,10 @@
+package ininamevalue
+
+import (
+	"sourcecode.social/reiver/go-erorr"
+)
+
+const (
+	errNilRuneScanner = erorr.Error("ini: nil rune scanner")
+	errNilRuneWriter  = erorr.Error("ini: nil rune writer")
+)
---
+++ internal/namevalue/readrune.go
@@ -0,0 +1,23 @@
+package ininamevalue
+
+import (
+	"io"
+
+	"sourcecode.social/reiver/go-erorr"
+)
+
+func readrune(runeScanner io.RuneScanner) (rune, error) {
+
+	r, size, err := runeScanner.ReadRune()
+	if io.EOF == err {
+		return r, err
+	}
+	if nil != err {
+		return r, erorr.Errorf("ini: problem reading rune in name-value: %w", err)
+	}
+	if size <= 0 {
+		return r, erorr.Error("ini: problem reading rune in name-value")
+	}
+
+	return r, err
+}
