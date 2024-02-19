d8f648467b9ab7375b30d96c4b1442362b15c840
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-02-18 21:58:27 -0800

    git rm

--- marshaler.go
+++
@@ -1,6 +0,0 @@
-package ini
-
-// Marshaler is the interface implemented by types that can marshal themselves into valid INI.
-type Marshaler interface {
-	MarshalINI() ([]byte, error)
-}
--- unmarshaler.go
+++
@@ -1,8 +0,0 @@
-package ini
-
-// Unmarshaler is the interface implemented by types that can unmarshal an INI description of themselves.
-// The input can be assumed to be a valid encoding of an INI value.
-// UnmarshalINI must copy the INI data if it wishes to retain the data after returning.
-type Unmarshaler interface {
-	UnmarshalINI([]byte) error
-}
