6c01b809de6051dafa194c4fb6cdd03caf4404af
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-02-20 00:55:29 -0800

    errors

--- errors.go
+++ errors.go
@@ -5,5 +5,11 @@ import (
 )
 
 const (
-	errNilRuneScanner = erorr.Error("ini: nil rune-scanner")
+	errNilBytes      = erorr.Error("ini: nil bytes")
+	errInternalError = erorr.Error("ini: internal error")
+	errNilMap        = erorr.Error("ini: nil map")
+	errNilPublisher  = erorr.Error("ini: nil publisher")
+	errNilReceiver   = erorr.Error("ini: nil receiver")
+	errNilSetter     = erorr.Error("ini: nil setter")
+	errRuneError     = erorr.Error("ini: rune error")
 )
