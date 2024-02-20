9c11469435ed4484ded66c5b092db922cb71f975
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-02-19 22:38:09 -0800

    ini section

--- internal/section/errors.go
+++ internal/section/errors.go
@@ -5,8 +5,6 @@ import (
 )
 
 const (
-	errEmptyString    = erorr.Error("ini: empty string")
-	errMagicNotFound  = erorr.Error("ini: section magic character (i.e., '[' (U+005B)) not found")
-	errNilRuneScanner = erorr.Error("ini: nil rune scanner")
-	errNilRuneWriter  = erorr.Error("ini: nil rune writer")
+	errInternalError  = erorr.Error("ini: internal error")
+	errRuneError      = erorr.Error("ini: rune error")
 )
