bc3c1edaf05ba0f7606908acdb78a659e1876cfe
Charles Iliya Krempeaux <iamreiver@gmail.com>
2025-01-02 13:19:16 -0800

    docs

--- valuer.go
+++ valuer.go
@@ -1,6 +1,18 @@
 package ini
 
+// Valuer is used by custom types to return their INI value.
+//
 // Valuer is used by [ValueOf]
+//
+// For example:
+//
+//	type MyStruct struct {
+//		// ..
+//	}
+//	
+//	func (recevier MyStruct) INIValue() (string, error) {
+//		// ...
+//	}
 type Valuer interface {
 	INIValue() (string, error)
 }
