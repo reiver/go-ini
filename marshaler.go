9fb5a6ff8376703081445052e06405d7b8523a75
Charles Iliya Krempeaux <iamreiver@gmail.com>
2025-01-03 11:25:46 -0800

    docs

--- marshaler.go
+++ marshaler.go
@@ -1,8 +1,13 @@
 package ini
 
-// Marshaler is used by custom types to return its INI value.
+// Marshaler is used by non-simple custom types to return its INI value.
 //
 // Behind the scenes, Marshaler is used by [Marshaler]
+//
+// If you are implmenting your own MarshalINI method for your own custom type, then you are likely going to use one or more of these functions:
+// [Append], [AppendKeyValue], [AppendSectionHeader], [AppendSequenceHeader].
+//
+// Note that if your custom-type is instead a simple custom type, then it should instead implement [Valuer] instead of Marshaler.
 type Marshaler interface {
 	MarshalINI([]byte, ...string) ([]byte, error)
 }
