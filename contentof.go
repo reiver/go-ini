5f3b0bf470eff0853a621f14f4c9a651a2243582
Charles Iliya Krempeaux <iamreiver@gmail.com>
2025-01-02 23:24:09 -0800

    docs

--- contentof.go
+++ contentof.go
@@ -6,7 +6,7 @@ import (
 
 // ContentOf returns the ini-content of a type, if it has one.
 //
-// Some Go built-in types have an ini-content represention; such as map[string]any
+// Some Go built-in types have an ini-content represention; such as map[string]any and map[string]string
 //
 // A custom type can also have an ini-content by implementing the [Contenter] interface.
 func ContentOf(v any, nesting ...string) ([]byte, error) {
