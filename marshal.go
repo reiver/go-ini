b87c422d11e979236c89a2004fca989b10d0845a
Charles Iliya Krempeaux <iamreiver@gmail.com>
2025-01-02 23:25:13 -0800

    docs

--- marshal.go
+++ marshal.go
@@ -3,6 +3,8 @@ package ini
 // Marshal returns the INI encoding of `value`.
 //
 // See also [ToString] and [Write]
+//
+// Notes that, Marshal is a wrapper around [ContentOf] with the 'nesting' parameter being empty.
 func Marshal(value any) ([]byte, error) {
 	return ContentOf(value)
 }
