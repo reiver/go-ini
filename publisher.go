1b8d96ee1ad0181567161460c83a236b867d0b34
Charles Iliya Krempeaux <iamreiver@gmail.com>
2025-01-16 17:12:03 -0800

    correction (was missing comma)

--- publisher.go
+++ publisher.go
@@ -2,7 +2,7 @@ package ini
 
 type Publisher interface {
 	PublishINIComment(comment string) error
-	PublishINIKeyValue(string string) error
+	PublishINIKeyValue(string, string) error
 	PublishINISectionHeader(...string) error
 	PublishINISequenceHeader(...string) error
 }
