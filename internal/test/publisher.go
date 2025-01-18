36480528f5d8c044facb52a7f4ba8bd184052d67
Charles Iliya Krempeaux <iamreiver@gmail.com>
2025-01-18 06:14:28 -0800

    initest.Publisher

--- internal/test/publisher.go
+++ internal/test/publisher.go
@@ -8,17 +8,29 @@ type Publisher struct {
 }
 
 func (receiver Publisher) PublishINIComment(comment string) error {
+	if nil == receiver.PublishINICommentFunc {
+		return nil
+	}
 	return receiver.PublishINICommentFunc(comment)
 }
 
 func (receiver Publisher) PublishINIKeyValue(key string, value string) error {
+	if nil == receiver.PublishINIKeyValueFunc {
+		return nil
+	}
 	return receiver.PublishINIKeyValueFunc(key, value)
 }
 
 func (receiver Publisher) PublishINISectionHeader(name ...string) error {
+	if nil == receiver.PublishINISectionHeaderFunc {
+		return nil
+	}
 	return receiver.PublishINISectionHeaderFunc(name...)
 }
 
 func (receiver Publisher) PublishINISequenceHeader(name ...string) error {
+	if nil == receiver.PublishINISequenceHeaderFunc {
+		return nil
+	}
 	return receiver.PublishINISequenceHeaderFunc(name...)
 }
